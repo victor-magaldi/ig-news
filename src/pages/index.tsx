5c05afb196eb95643246f3c03ab92d27d934348a
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-17 21:29:44 -0300

    Styles Global reset

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import style from "../styles/home.module.scss";
 import Head from "next/head";
 
 export default function Home() {
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>ig.News - Home</title>
       </Head>
-      <h1 className={style.title}>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   );
 }
