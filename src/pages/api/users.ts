3bb4cbaf299275887c2c7c5f4250cf479e04bb9c
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-21 21:38:26 -0300

    api in next js

---
+++ src/pages/api/users.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+
+export default function users(request: NextApiRequest, response: NextApiResponse) {
+
+    const users = [
+        {
+            id: 1,
+            name: "Victor"
+        },
+        {
+            id: 2,
+            name: "teste"
+        }
+    ]
+
+    return response.json(users)
+}
