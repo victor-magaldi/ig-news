3ee2aaef248594c6ebfb46c271cec68999e0c986
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-26 20:22:40 -0300

    create session stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,12 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client";
+
 import { stripe } from "../../services/stripe";
 
 export default async function subscribe(
-    request: NextApiRequest,
-    response: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-    if (request.method === "POST") {
-        const checkoutSession = await stripe.checkout.sessions.create({
+    if (req.method === "POST") {
+        const session = await getSession({ req })
+
+        const stripeCustomer = await stripe.customers.create({
+            email: session.user.email,
+        })
+        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+            customer: stripeCustomer.id,
             payment_method_types: ["card"],
             billing_address_collection: "required",
             line_items: [
@@ -17,11 +25,10 @@ export default async function subscribe(
             success_url: process.env.STRIPE_SUCCESS_URL,
             cancel_url: process.env.STRIPE_CANCEL_URL,
         })
+
+        return res.status(200).json({ sessionId: stripeCheckoutSession.id })
     } else {
-        response.setHeader("allow", "POST")
-        response.status(405).end("method not allowed")
+        res.setHeader("allow", "POST")
+        res.status(405).end("method not allowed")
     }
-    return response.json({
-        ping: true,
-    });
 }
