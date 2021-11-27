8d210fcf23dba6c08108fdc824f97cc8da358346
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-26 21:18:23 -0300

    evitando customers duplicados

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,34 +1,75 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
+import { Casefold, query as q } from "faunadb"
+
 
 import { stripe } from "../../services/stripe";
 
+type User = {
+  ref: { id: string },
+  data: {
+    stripe_customer_id: string
+  }
+}
 export default async function subscribe(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    if (req.method === "POST") {
-        const session = await getSession({ req })
+  if (req.method === "POST") {
+    const session = await getSession({ req })
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-        })
-        const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
-            payment_method_types: ["card"],
-            billing_address_collection: "required",
-            line_items: [
-                { price: "price_1JyOpKHZlUJYmkb8GPFgnfFD", quantity: 1 }
-            ],
-            mode: "subscription",
-            allow_promotion_codes: true,
-            success_url: process.env.STRIPE_SUCCESS_URL,
-            cancel_url: process.env.STRIPE_CANCEL_URL,
-        })
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index("user_by_email"),
+          Casefold(session.user.email)
+        )
+      )
+    )
 
-        return res.status(200).json({ sessionId: stripeCheckoutSession.id })
-    } else {
-        res.setHeader("allow", "POST")
-        res.status(405).end("method not allowed")
+    let customerId = user.data?.stripe_customer_id
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      })
+
+
+      await fauna.query(
+        q.Update(
+          q.Ref(
+            q.Collection("users"),
+            user.ref.id,
+          ),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
+      customerId = stripeCustomer.id
     }
+
+
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [
+        { price: "price_1JyOpKHZlUJYmkb8GPFgnfFD", quantity: 1 }
+      ],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    })
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+  } else {
+    res.setHeader("allow", "POST")
+    res.status(405).end("method not allowed")
+  }
 }
