4d94305bd2ee9804251eb3cb7c0cd865b687b494
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-27 13:38:47 -0300

    webhooks

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function subscribe(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    console.log("evento recebido")
+
+    res.status(200).json({ ok: true })
+}
