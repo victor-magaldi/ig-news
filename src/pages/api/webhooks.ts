bf45a25a68a16a4b89dfe2c6be545e8b2c2a3de0
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-29 21:24:28 -0300

    init buffer function

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,10 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+
+async function buffer(readable: Readable) {
+    const chunks = []
+    for await (const chunk of readable) {
+        chunks.push(
+            typeof chunk === "string" ? Buffer.from(chunk) : chunk
+        )
+    }
+    return Buffer.concat(chunks)
+}
 
 export default async function subscribe(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    console.log("evento recebido")
+    const buf = await buffer(req)
 
     res.status(200).json({ ok: true })
 }
