27d3f1198eb6d8784bdc7dd3dfc21907cb47e842
victor de souza magaldi <victmagaldi@hotmail.com>
2021-12-01 21:35:26 -0300

    webhooks

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -3,7 +3,7 @@ import { stripe } from "../../../services/stripe";
 
 import { query as q } from "faunadb"
 
-export async function saveSubscription(subscriptionId: string, customerId: string) {
+export async function saveSubscription(subscriptionId: string, customerId: string, createAction: boolean = false) {
 
   const userRef = await fauna.query(
     q.Select(
@@ -25,16 +25,40 @@ export async function saveSubscription(subscriptionId: string, customerId: strin
     price_id: subscription.items.data[0].price.id,
 
   }
-  try {
-    await fauna.query(
-      q.Create(
-        q.Collection("subscriptions"),
-        { data: subscriptionData }
+
+
+  if (createAction) {
+    try {
+      await fauna.query(
+        q.Create(
+          q.Collection("subscriptions"),
+          { data: subscriptionData }
+        )
       )
-    )
-  } catch (err) {
-    console.log("error", err)
+    } catch (err) {
+      console.log("error", err)
+    }
+  } else {
+    try {
+      console.log
+      await fauna.query(
+        q.Replace(
+          q.Select(
+            "ref",
+            q.Get(
+              q.Match(
+                q.Index("subscription_by_id"),
+                subscriptionId
+              )
+            )
+          ),
+          {
+            data: subscriptionData,
+          }
+        )
+      )
+    } catch (err) {
+      console.log("error", err)
+    }
   }
-
-
 }
