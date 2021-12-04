b5705c608a77ad34588692302ccfff700c854623
victor de souza magaldi <victmagaldi@hotmail.com>
2021-12-03 23:06:38 -0300

    init page posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import React from "react";
+import style from "./style.module.scss";
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | IgNews</title>
+      </Head>
+      <main>
+        <div>
+          <a href="">
+            <time>17 de março</time>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
