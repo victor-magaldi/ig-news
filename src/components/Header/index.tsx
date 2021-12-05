e0bc2dabfd82d824fc996d01a6630b2b49c49de9
victor de souza magaldi <victmagaldi@hotmail.com>
2021-12-05 20:27:10 -0300

    Link next

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 // import Image from "next/image";
 import { SignInButton } from "../SignInButton";
+import Link from "next/link";
 import styles from "./style.module.scss";
 
 export function Header() {
@@ -8,10 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig news" />
         <nav>
-          <a href="" className={styles.active}>
-            Home
-          </a>
-          <a href="">Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a>Posts</a>
+          </Link>
         </nav>
         <SignInButton />
       </div>
