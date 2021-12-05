da5e587c9eb68178d9e06848c7222c18b38ad44c
victor de souza magaldi <victmagaldi@hotmail.com>
2021-12-04 22:20:03 -0300

    styles and structure posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -8,10 +8,41 @@ export default function Posts() {
       <Head>
         <title>Posts | IgNews</title>
       </Head>
-      <main>
-        <div>
-          <a href="">
+      <main className={style.container}>
+        <div className={style.posts}>
+          <a href="#">
             <time>17 de março</time>
+            <strong>Tille my post is this</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
+              laudantium illo aspernatur quis incidunt adipisci debitis ullam
+              amet odit ducimus magnam iusto voluptatibus officia nemo atque
+              alias aut, doloribus facere. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Magnam, iure, voluptatum beatae
+              architecto cumque harum explicabo recusandae, quibusdam porro
+              possimus fugit nostrum tenetur earum animi itaque saepe numquam
+              aliquam doloremque. Cumque reprehenderit nemo quis ut hic velit
+              sunt, similique perspiciatis quasi perferendis vel labore dicta
+              iste quisquam iusto cupiditate placeat quae? Repellendus magni id
+              corporis rerum ipsum reprehenderit eum atque!
+            </p>
+          </a>
+          <a href="#">
+            <time>17 de março</time>
+            <strong>Tille my post is this</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
+              laudantium illo aspernatur quis incidunt adipisci debitis ullam
+              amet odit ducimus magnam iusto voluptatibus officia nemo atque
+              alias aut, doloribus facere. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Magnam, iure, voluptatum beatae
+              architecto cumque harum explicabo recusandae, quibusdam porro
+              possimus fugit nostrum tenetur earum animi itaque saepe numquam
+              aliquam doloremque. Cumque reprehenderit nemo quis ut hic velit
+              sunt, similique perspiciatis quasi perferendis vel labore dicta
+              iste quisquam iusto cupiditate placeat quae? Repellendus magni id
+              corporis rerum ipsum reprehenderit eum atque!
+            </p>
           </a>
         </div>
       </main>
